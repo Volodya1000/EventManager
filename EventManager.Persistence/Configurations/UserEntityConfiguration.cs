521c7369a011a18170d20b2ff9aac57217541795
volodya.dichkovski <volodya.dichkovski@gmail.com>
2025-04-09 23:36:19 +0300

    Changes in entities

--- EventManager.Persistence/Configurations/UserEntityConfiguration.cs
+++ EventManager.Persistence/Configurations/UserEntityConfiguration.cs
@@ -12,6 +12,21 @@ public class UserEntityConfiguration : IEntityTypeConfiguration<UserEntity>
 
         builder.HasKey(u => u.Id);
         builder.Property(u => u.Id).ValueGeneratedOnAdd();
+      
+        builder.Property(u => u.FirstName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(u => u.LastName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(u => u.DateOfBirth)
+            .IsRequired();
+
+        builder.Property(u => u.RefreshToken);
+
+        builder.Property(u => u.RefreshTokenExpiresAtUtc);
 
         builder.HasMany(u => u.Participants)
             .WithOne(p => p.User)
--- EventManager.Persistence/Entities/UserEntity.cs
+++ EventManager.Persistence/Entities/UserEntity.cs
@@ -7,6 +7,8 @@ public class UserEntity : IdentityUser
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public DateTime DateOfBirth { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiresAtUtc { get; set; }
 
     public ICollection<ParticipantEntity> Participants { get; set; } = new List<ParticipantEntity>();
 }
