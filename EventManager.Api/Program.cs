92d7c5af849256520d47910f63ed719c28fcf260
volodya.dichkovski <volodya.dichkovski@gmail.com>
2025-04-10 13:05:53 +0300

    Add DbContext and Services.AddIdentity to program.cs

--- EventManager.Api/Program.cs
+++ EventManager.Api/Program.cs
@@ -1,6 +1,29 @@
+using EventManager.Domain.Models;
+using EventManager.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+// Добавление Identity для пользователя с настройками безопасности пароля
+// и уникальности электронной почты,
+// а также подключение к базе данных через Entity Framework.
+builder.Services.AddIdentity<User, IdentityRole<Guid>>(opt =>
+{
+    opt.Password.RequireDigit = true;
+    opt.Password.RequireLowercase = true;
+    opt.Password.RequireNonAlphanumeric = true;
+    opt.Password.RequireUppercase = true;
+    opt.Password.RequiredLength = 8;
+    opt.User.RequireUniqueEmail = true;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DbConnectionString"));
+});
 
 var app = builder.Build();
 
