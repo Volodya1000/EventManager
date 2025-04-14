44e4a7b5380eb8f71468ea9ef92c01226c859eda
volodya.dichkovski <volodya.dichkovski@gmail.com>
2025-04-14 17:47:18 +0300

    Added CreateEventRequestValidator

---
+++ EventManager.Application/Validators/CreateEventRequestValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+
+using EventManager.Application.Requests;
+
+namespace EventManager.Application.Validators;
+
+public class CreateEventRequestValidator : AbstractValidator<CreateEventRequest>
+{
+    public CreateEventRequestValidator()
+    {
+        RuleFor(r => r.Name)
+            .NotEmpty();
+
+        RuleFor(r => r.Description)
+            .NotEmpty();
+
+        RuleFor(r => r.DateTime)
+            .GreaterThan(DateTime.Now);
+
+        RuleFor(r => r.Location)
+            .NotEmpty();
+
+        RuleFor(r => r.Category)
+            .NotEmpty();
+
+        RuleFor(r => r.MaxParticipants).GreaterThan(0);
+
+        //список url либо пустой, либо все url валидны
+        RuleFor(r => r.ImageUrls)
+           .Must(list => list == null || list.All(url => Uri.TryCreate(url, UriKind.Absolute, out _)))
+           .WithMessage("All url should be valid");
+    }
+}
